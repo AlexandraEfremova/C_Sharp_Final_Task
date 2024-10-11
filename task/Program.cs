ea7a034bb9b86ccb5b4b894c3d40b48aa6d67bac
Efremova Aleksandra <doctor.akab@gmail.com>
2024-10-11 20:48:55 +0300

    создание метода на вывод элементов из массива"

--- task/Program.cs
+++ task/Program.cs
@@ -6,14 +6,29 @@
         System.Console.Write(i + ", ");
     System.Console.WriteLine();
     int number = GenerateNumber(0, 4);
-
+    int lengthArray = array.Length;
+    NewArray(array, number, lengthArray);
 }
 
+// метод генерации числа
 int GenerateNumber(int firstVal = 0, int secondVal = 4)
 {
     Random rnd = new Random();
     int number = rnd.Next(firstVal, secondVal);
     return number;
+}
+
+// метод вывода рандомных элементов из массива в количестве сгенерированного числа
+void NewArray(string[] array, int number, int lengthArr)
+{
+    Random rnd = new Random();
+    System.Console.Write("[");
+    for (int count = 0; count < number; count++)
+    {
+        string element = array[rnd.Next(0, lengthArr)];
+        Console.Write(element + ", ");
+    }
+    System.Console.Write("]");
 }
 
 Main();
