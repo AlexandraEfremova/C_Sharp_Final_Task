087069e5dba37da8c62e2014ddbdcf0ce63794dd
Efremova Aleksandra <doctor.akab@gmail.com>
2024-10-11 20:46:32 +0300

    создание метода на генерацию числа от 0 до 3

--- task/Program.cs
+++ task/Program.cs
@@ -4,8 +4,17 @@
     string[] array = {"Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
     foreach(string i in array)
         System.Console.Write(i + ", ");
+    System.Console.WriteLine();
+    int number = GenerateNumber(0, 4);
+
 }
 
+int GenerateNumber(int firstVal = 0, int secondVal = 4)
+{
+    Random rnd = new Random();
+    int number = rnd.Next(firstVal, secondVal);
+    return number;
+}
 
 Main();
 
